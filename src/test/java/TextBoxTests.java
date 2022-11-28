c08cc572fe8150d30b87319511585fa5e580410c
Dmitry Shelkovnikov <misslook3@gmail.com>
2022-11-28 19:39:49 +0300

    HW6 is done

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,5 +1,13 @@
+import com.github.javafaker.Faker;
 import tests.TestBase;
 import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Random;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 
 
 public class TextBoxTests extends TestBase {
@@ -7,18 +15,23 @@ public class TextBoxTests extends TestBase {
 
     @Test
     void fillFormTest() {
-        String userName = "Dmitry";
-        String LastName = "Shelkovnikov";
-        String Email = "qwe3@mail.com";
-        String Number = "1234567899";
-        String BirthDay = ("30");
-        String BirthMonth = ("May");
-        String BirthYear = ("2008");
+        Faker faker = new Faker(new Locale("eng"));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM yyyy", Locale.CANADA);
+        String[] birthday = sdf.format(faker.date().birthday()).split(" ");
+
+
+        String userName = faker.name().firstName();
+        String LastName = faker.name().lastName();
+        String Email = faker.internet().emailAddress();
+        String Number = faker.phoneNumber().subscriberNumber(10);
+        String BirthDay = birthday[0];
+        String BirthMonth = birthday[1];
+        String BirthYear = birthday[2];
         String Gender = "Male";
         String Subject = "English";
         String Hobbies = "Sports";
         String FileUpload = "Photo.jpg";
-        String currentAdress = "Miami/HomeBRB";
+        String currentAdress = faker.address().fullAddress();
         String state = "NCR";
         String city = "Delhi";
 
